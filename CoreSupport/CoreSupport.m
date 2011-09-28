afc444e15bc6a72b62c0933a7e082469ed406e94
Mike Mayo <greenisus@gmail.com>
2011-09-28 10:42:19 -0700

    make CoreSupport ARC-compliant

--- CoreSupport/CoreSupport.m
+++ CoreSupport/CoreSupport.m
@@ -20,8 +20,8 @@
         sortDescriptors = [NSMutableArray arrayWithCapacity:[sortChunks count] / 2];
         for (int chunkIdx = 0; chunkIdx < [sortChunks count]; chunkIdx += 2) {
             [sortDescriptors addObject:
-			 [[[NSSortDescriptor alloc] initWithKey:[sortChunks objectAtIndex:chunkIdx] ascending:
-			   [[sortChunks objectAtIndex:chunkIdx + 1] caseInsensitiveCompare:@"asc"] == NSOrderedSame] autorelease]];
+			 [[NSSortDescriptor alloc] initWithKey:[sortChunks objectAtIndex:chunkIdx] ascending:
+			   [[sortChunks objectAtIndex:chunkIdx + 1] caseInsensitiveCompare:@"asc"] == NSOrderedSame]];
         }
     }
     return sortDescriptors;
